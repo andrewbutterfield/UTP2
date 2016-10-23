16380647bc2b90e75b40f0bd43d7987deaef4c59
Andrew Butterfield <Andrew.Butterfield@scss.tcd.ie>
2016-10-23 23:28:20 +0100

    progressing analysis

--- design/ModuleDeps.hs
+++ design/ModuleDeps.hs
@@ -2,6 +2,10 @@ module ModuleDeps where
 import System.Environment
 import qualified Data.Map as M
 
+type Tree = M.Map String [String]
+
+mrgTree = M.unionWith (++)
+
 main 
  = do args <- getArgs
       let (lhsLog,importLog,importTree) = parse args
@@ -25,20 +29,36 @@ parseLHS = pLHS . reverse
   pLHS ('s':'h':'l':'.':rest) = reverse $ takeWhile (/='/') rest
   pLHS _ = ""
 
+parseLHSs = M.fromList . map addNull . filter (/="") . map parseLHS
+addNull str= (str,[""])
+
+
 -- ../src/DataText.lhs:19:import Text.ParserCombinators.Parsec.Token as P
+parseImport :: String -> Tree
 parseImport = pImp . words
  where
-  pImp (w1:w2:_) = M.singleton w1 [w2]
+  pImp (w1:w2:_) 
+   = case pImp' w1 w2 of
+      Nothing -> M.empty
+      Just (m1,m2) -> M.singleton m1 [m2]
   pImp _ = M.empty
 
-type Tree = M.Map String [String]
+  pImp' :: String -> String -> Maybe (String, String)
+  pImp' w1 w2
+   = do parentMod <- parseParent w1
+        childMod <- parseChild w2
+        return (parentMod,childMod)
 
-parseLHSs = M.fromList . map addNull . filter (/="") . map parseLHS
-addNull str= (str,"")
+-- ../src/DataText.lhs:19:import
+parseParent w1 = return w1
 
+-- Text.ParserCombinators.Parsec.Token as P
+parseChild w2 = return w2
+
+parseImports = foldl mrgTree M.empty . map parseImport
 
 buildTree lhs imports 
-  = parseLHSs lhs
+  = parseLHSs lhs `mrgTree` parseImports imports
 
 treeShow = show
 
